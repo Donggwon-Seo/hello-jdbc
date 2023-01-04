4dcbe634852f293b4950aec49577dc95e9bcdc27
SeoDonggwon <ehdrnjsdl920@naver.com>
2023-01-04 23:09:59 +0900

    3주차 과제 코드

--- src/main/java/io/wisoft/homework/week3/EmployeeSelectService.java
+++ src/main/java/io/wisoft/homework/week3/EmployeeSelectService.java
@@ -18,7 +18,7 @@ public class EmployeeSelectService {
             conn = PostgresqlAccess.setConnection();
             conn.setAutoCommit(false);
 
-            String query = "SELECT * FROM EMPLOYEE";
+            String query = "SELECT * FROM EMPLOYEE ";
             pstmt = conn.prepareStatement(query);
 
             rs = pstmt.executeQuery();
@@ -57,4 +57,169 @@ public class EmployeeSelectService {
             }
         }
     }
+
+    public void getEmployeeBycode(String code) {
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = PostgresqlAccess.setConnection();
+            conn.setAutoCommit(false);
+
+            String query = "SELECT * FROM EMPLOYEE WHERE emp_code = ?";
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, code);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                System.out.print("[직원코드]: " + rs.getString(1));
+                System.out.print(" | [이름]: " + rs.getString(2));
+                System.out.print(" | [매니저코드]: " + rs.getString(3));
+                System.out.print(" | [월급]: " + rs.getInt(4));
+                System.out.println(" | [직책코드]: " + rs.getString(5));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+
+            if (rs != null) {
+                try { rs.close(); } catch (Exception e) { e.printStackTrace();}
+            }
+
+            if (pstmt != null) {
+                try { pstmt.close(); } catch (Exception e) { e.printStackTrace();}
+            }
+
+            if (conn != null) {
+                try { conn.close(); } catch (Exception e) { e.printStackTrace();}
+            }
+        }
+    }
+
+    public void getEmployeeByName(String name) {
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = PostgresqlAccess.setConnection();
+            conn.setAutoCommit(false);
+
+            String query = "SELECT * FROM EMPLOYEE WHERE emp_name = ?";
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, name);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                System.out.print("[직원코드]: " + rs.getString(1));
+                System.out.print(" | [이름]: " + rs.getString(2));
+                System.out.print(" | [매니저코드]: " + rs.getString(3));
+                System.out.print(" | [월급]: " + rs.getInt(4));
+                System.out.println(" | [직책코드]: " + rs.getString(5));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+
+            if (rs != null) {
+                try { rs.close(); } catch (Exception e) { e.printStackTrace();}
+            }
+
+            if (pstmt != null) {
+                try { pstmt.close(); } catch (Exception e) { e.printStackTrace();}
+            }
+
+            if (conn != null) {
+                try { conn.close(); } catch (Exception e) { e.printStackTrace();}
+            }
+        }
+    }
+
+    public void getEmployeeByMgt(String mgt) {
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = PostgresqlAccess.setConnection();
+            conn.setAutoCommit(false);
+
+            String query = "SELECT * FROM EMPLOYEE WHERE emp_mgt = ?";
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, mgt);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                System.out.print("[직원코드]: " + rs.getString(1));
+                System.out.print(" | [이름]: " + rs.getString(2));
+                System.out.print(" | [매니저코드]: " + rs.getString(3));
+                System.out.print(" | [월급]: " + rs.getInt(4));
+                System.out.println(" | [직책코드]: " + rs.getString(5));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+
+            if (rs != null) {
+                try { rs.close(); } catch (Exception e) { e.printStackTrace();}
+            }
+
+            if (pstmt != null) {
+                try { pstmt.close(); } catch (Exception e) { e.printStackTrace();}
+            }
+
+            if (conn != null) {
+                try { conn.close(); } catch (Exception e) { e.printStackTrace();}
+            }
+        }
+    }
+
+    public void getEmployeeByRcode(String rcode) {
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = PostgresqlAccess.setConnection();
+            conn.setAutoCommit(false);
+
+            String query = "SELECT * FROM EMPLOYEE WHERE emp_rcode = ?";
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, rcode);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                System.out.print("[직원코드]: " + rs.getString(1));
+                System.out.print(" | [이름]: " + rs.getString(2));
+                System.out.print(" | [매니저코드]: " + rs.getString(3));
+                System.out.print(" | [월급]: " + rs.getInt(4));
+                System.out.println(" | [직책코드]: " + rs.getString(5));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+
+            if (rs != null) {
+                try { rs.close(); } catch (Exception e) { e.printStackTrace();}
+            }
+
+            if (pstmt != null) {
+                try { pstmt.close(); } catch (Exception e) { e.printStackTrace();}
+            }
+
+            if (conn != null) {
+                try { conn.close(); } catch (Exception e) { e.printStackTrace();}
+            }
+        }
+    }
+
 }
--- src/main/java/io/wisoft/homework/week3/Main.java
+++ src/main/java/io/wisoft/homework/week3/Main.java
@@ -4,9 +4,28 @@ public class Main {
     public static void main(String[] args) {
         EmployeeSelectService employeeSelectService = new EmployeeSelectService();
 
-        System.out.println("전체 직원을 검색");
+        System.out.println("전체 직원 검색");
         employeeSelectService.getEmployeeAll();
         System.out.println();
 
+        System.out.println("직원코드가 E101인 직원을 검색");
+        String code = "E101";
+        employeeSelectService.getEmployeeBycode(code);
+        System.out.println();
+
+        System.out.println("이름이 이순신인 직원을 검색");
+        String name = "이순신";
+        employeeSelectService.getEmployeeByName(name);
+        System.out.println();
+
+        System.out.println("매니저가 E101인 직원을 검색");
+        String mgt = "E101";
+        employeeSelectService.getEmployeeByMgt(mgt);
+        System.out.println();
+
+        System.out.println("직책 코드가 R001인 직원을 검색");
+        String rcode = "R001";
+        employeeSelectService.getEmployeeByRcode(rcode);
+        System.out.println();
     }
 }
