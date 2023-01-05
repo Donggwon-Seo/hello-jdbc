26a87b1bbd7d660170f642f5d331f866c50241e5
SeoDonggwon <ehdrnjsdl920@naver.com>
2023-01-05 17:58:43 +0900

    DELETE 구현

--- src/main/java/io/wisoft/seminar/StudentDeleteService.java
+++ src/main/java/io/wisoft/seminar/StudentDeleteService.java
@@ -16,8 +16,8 @@ public class StudentDeleteService {
             String query = "DELETE FROM STUDENT WHERE NO = ?";
             pstmt = conn.prepareStatement(query);
 
-            for(int i = 0; i<students.length; i++){
-                if(students[i].getNo() == null)
+            for (int i = 0; i < students.length; i++) {
+                if (students[i].getNo() == null)
                     break;
 
                 pstmt.setString(1, students[i].getNo());
@@ -51,7 +51,39 @@ public class StudentDeleteService {
             }
         }
     }
-    public void deleteStudentNo(String studentNo) {
 
+    public void deleteStudentNo(String studentNo) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = PostgresqlAccess.setConnection();
+            conn.setAutoCommit(false);
+            String query = "DELETE FROM STUDENT WHERE NO = ?";
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, studentNo);
+            int retValue = pstmt.executeUpdate();
+            conn.commit();
+            System.out.println(retValue + "건의 사항이 처리되었습니다.");
+        } catch (SQLException sqex) {
+            try {
+                if (conn != null) {
+                    conn.rollback();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            System.out.println("SQLException = " + sqex.getMessage());
+            System.out.println("SQLState = " + sqex.getSQLState());
+        } finally {
+            if (pstmt != null) try {
+                pstmt.close();
+            } catch (Exception e) {
+            }
+
+            if (conn != null) try {
+                conn.close();
+            } catch (Exception e) {
+            }
+        }
     }
 }
