31932bf804cad9501e9576f9bc243a60c4980140
SeoDonggwon <ehdrnjsdl920@naver.com>
2023-01-05 17:56:04 +0900

    UPDATE DELETE TEST

--- src/main/java/io/wisoft/seminar/Main.java
+++ src/main/java/io/wisoft/seminar/Main.java
@@ -8,6 +8,8 @@ public class Main {
         Student student = new Student();
         Student[] students = new Student[8];
         StudentInsertService insertService = new StudentInsertService();
+        StudentUpdateService studentUpdateService = new StudentUpdateService();
+        StudentDeleteService studentDeleteService = new StudentDeleteService();
 
         for (int i = 0; i < students.length; i++) {
             students[i] = new Student();
@@ -23,9 +25,70 @@ public class Main {
 //         insertMultiBatchTest(students, insertService);
 
 //         UPDATE AREA
-        StudentUpdateService studentUpdateService = new StudentUpdateService();
 
+//        updateTest(studentUpdateService);
+//        updateTest(student, studentUpdateService);
+//        updateBatchTest(students, studentUpdateService);
+
+//        DELETE AREA
+
+//        deleteBatchTest(students, studentDeleteService);
+
+
+    }
+
+    private static void deleteBatchTest(Student[] students, StudentDeleteService studentDeleteService) {
+        students[0].setNo("20110501");
+        students[1].setNo("20110601");
+        students[2].setNo("20110701");
+        students[3].setNo("20110801");
+        students[4].setNo("20110901");
+        students[5].setNo("20111001");
+        students[6].setNo("20111101");
+        students[7].setNo("20111201");
+
+        studentDeleteService.deleteStudentNoMultiBatch(students);
+        System.out.println();
+    }
+
+    private static void updateTest(StudentUpdateService studentUpdateService) {
         studentUpdateService.updateStudentBirthday("20110401", "1990-03-21");
+        System.out.println();
+    }
+
+    private static void updateTest(Student student, StudentUpdateService studentUpdateService) {
+        student.setNo("20110401");
+        student.setBirthday("1990-03-25");
+        studentUpdateService.updateStudentBirthday(student);
+        System.out.println();
+    }
+
+    private static void updateBatchTest(Student[] students, StudentUpdateService studentUpdateService) {
+        students[0].setNo("20110501");
+        students[0].setBirthday("1990-03-01");
+
+        students[1].setNo("20110601");
+        students[1].setBirthday("1990-04-01");
+
+        students[2].setNo("20110701");
+        students[2].setBirthday("1990-05-01");
+
+        students[3].setNo("20110801");
+        students[3].setBirthday("1990-06-01");
+
+        students[4].setNo("20110901");
+        students[4].setBirthday("1990-07-01");
+
+        students[5].setNo("20111001");
+        students[5].setBirthday("1990-08-01");
+
+        students[6].setNo("20111101");
+        students[6].setBirthday("1990-09-01");
+
+        students[7].setNo("20111201");
+        students[7].setBirthday("1990-10-01");
+
+        studentUpdateService.updateStudentBirthdayMultiBatch(students);
         System.out.println();
     }
 
